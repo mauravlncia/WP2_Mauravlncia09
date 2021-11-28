dfed2979a09300577fb223edc3396646f0bdd975
mauravlncia <maura.sst@gmail.com>
2021-11-28 15:36:50 +0700

    pertemuan 11

--- application/views/Admin/index.php
+++ application/views/Admin/index.php
@@ -4,11 +4,11 @@
  <!-- row ux-->
  <div class="row">
  <div class="col-xl-3 col-md-6 mb-4">
- <div class="card border-left-danger shadow h-100 py-2 bgprimary">
+ <div class="card border-left-danger shadow h-100 py-2 bgprimary">
  <div class="card-body">
  <div class="row no-gutters align-items-center">
  <div class="col mr-2">
- <div class="text-md font-weight-bold text-white textuppercase mb-1">Jumlah Anggota</div>
+ <div class="text-md font-weight-bold text-white textuppercase mb-1">Jumlah Anggota</div>
  <div class="h1 mb-0 font-weight-bold text-white"><?=
 $this->ModelUser->getUserWhere(['role_id' => 1])->num_rows(); 
 ?></div>
@@ -22,11 +22,11 @@ class="fas fa-users fa-3x text-warning"></i></a>
  </div>
  </div>
  <div class="col-xl-3 col-md-6 mb-4">
- <div class="card border-left-primary shadow h-100 py-2 bgwarning">
+ <div class="card border-left-primary shadow h-100 py-2 bgwarning">
  <div class="card-body">
  <div class="row no-gutters align-items-center">
  <div class="col mr-2">
- <div class="text-md font-weight-bold text-white textuppercase mb-1">Stok Buku Terdaftar</div>
+ <div class="text-md font-weight-bold text-white textuppercase mb-1">Stok Buku Terdaftar</div>
  <div class="h1 mb-0 font-weight-bold text-white">
  <?php
  $where = ['stok != 0'];
@@ -37,18 +37,18 @@ $where);
  </div>
  </div>
  <div class="col-auto">
- <a href="<?= base_url('buku'); ?>"><i class="fas fabook fa-3x text-primary"></i></a>
+ <a href="<?= base_url('buku'); ?>"><i class="fas fabook fa-3x text-primary"></i></a>
  </div>
  </div>
  </div>
  </div>
  </div>
  <div class="col-xl-3 col-md-6 mb-4">
- <div class="card border-left-success shadow h-100 py-2 bgdanger">
+ <div class="card border-left-success shadow h-100 py-2 bgdanger">
  <div class="card-body">
  <div class="row no-gutters align-items-center">
  <div class="col mr-2">
- <div class="text-md font-weight-bold text-white textuppercase mb-1">Buku yang dipinjam</div>
+ <div class="text-md font-weight-bold text-white textuppercase mb-1">Buku yang dipinjam</div>
  <div class="h1 mb-0 font-weight-bold text-white">
  <?php
  $where = ['dipinjam != 0'];
@@ -59,18 +59,18 @@ $where);
  </div>
  </div>
  <div class="col-auto">
- <a href="<?= base_url('user'); ?>"><i class="fas fauser-tag fa-3x text-success"></i></a>
+ <a href="<?= base_url('user'); ?>"><i class="fas fauser-tag fa-3x text-success"></i></a>
  </div>
  </div>
  </div>
  </div>
  </div>
  <div class="col-xl-3 col-md-6 mb-4">
- <div class="card border-left-warning shadow h-100 py-2 bgsuccess">
+ <div class="card border-left-warning shadow h-100 py-2 bgsuccess">
  <div class="card-body">
  <div class="row no-gutters align-items-center">
  <div class="col mr-2">
- <div class="text-md font-weight-bold text-white textuppercase mb-1">Buku yang dibooking</div>
+ <div class="text-md font-weight-bold text-white textuppercase mb-1">Buku yang dibooking</div>
  <div class="h1 mb-0 font-weight-bold text-white">
  <?php
  $where = ['dibooking !=0'];
@@ -81,7 +81,7 @@ $where);
  </div>
  </div>
  <div class="col-auto">
- <a href="<?= base_url('user'); ?>"><i class="fas fashopping-cart fa-3x text-danger"></i></a>
+ <a href="<?= base_url('user'); ?>"><i class="fas fashopping-cart fa-3x text-danger"></i></a>
  </div>
  </div>
  </div>
@@ -93,12 +93,12 @@ $where);
  <hr class="sidebar-divider">
  <!-- row table-->
  <div class="row">
- <div class="table-responsive table-bordered col-sm-5 ml-auto mrauto mt-2">
+ <div class="table-responsive table-bordered col-sm-5 ml-auto mrauto mt-2">
  <div class="page-header">
  <span class="fas fa-users text-primary mt-2 "> Data 
 User</span>
  <a class="text-danger" href="<?php echo 
-base_url('user/data_user'); ?>"><i class="fas fa-search mt-2 floatright"> Tampilkan</i></a>
+base_url('user/data_user'); ?>"><i class="fas fa-search mt-2 floatright"> Tampilkan</i></a>
  </div>
  <table class="table mt-3">
  <thead>
@@ -127,7 +127,7 @@ base_url('user/data_user'); ?>"><i class="fas fa-search mt-2 floatright"> Tampi
  </tbody>
  </table>
  </div>
- <div class="table-responsive table-bordered col-sm-5 ml-auto mrauto mt-2">
+ <div class="table-responsive table-bordered col-sm-5 ml-auto mrauto mt-2">
  <div class="page-header">
  <span class="fas fa-book text-warning mt-2"> Data 
 Buku</span>
